20ccbc488677c4feb2094dff230f68c08088ec11
Thotli Mohan Reddy <thotlimohan@moresandtechnologies.com>
2025-07-15 14:43:03 +0530

    fix004

--- HUX/HUXfrontend/ios/HUXSmartRingApp/AppDelegate.swift
+++ HUX/HUXfrontend/ios/HUXSmartRingApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "HUXfrontend",
+      withModuleName: "HUXSmartRingApp",
       in: window,
       launchOptions: launchOptions
     )
